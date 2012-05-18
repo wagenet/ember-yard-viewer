b6d97e103f4296756e5bc8770e73a5388b74c7cb
Peter Wagenet <peter.wagenet@gmail.com>
2012-05-18 16:59:49 -0700

    Show methods

--- app.js
+++ app.js
@@ -2,17 +2,45 @@ YardViewer = Ember.Application.create({
 
   load: function(data) {
     data["@graph"].forEach(function(item) {
-      if (item.type === 'module') {
-        var module = YardViewer.Module.create(item);
-        YardViewer.modulesController.pushObject(module);
+      switch(item.type) {
+        case 'module':
+          YardViewer.modulesController.createModule(item);
+          break;
+        case 'method':
+          var module = YardViewer.modulesController.findProperty('path', item.namespace);
+          if (module) {
+            module.createMethod(item);
+          } else {
+            console.warn("Unable to find namespace: "+item.namespace);
+          }
+          break;
       }
     });
   }
 
 });
 
-YardViewer.Module = Ember.Object.extend();
+YardViewer.Module = Ember.Object.extend({
+  methods: null,
+
+  createMethod: function(data) {
+    var method = YardViewer.Method.create(data);
+    this.get('methods').pushObject(method);
+  },
+
+  init: function() {
+    this._super();
+    this.set('methods', []);
+  }
+});
+
+YardViewer.Method = Ember.Object.extend();
 
 YardViewer.modulesController = Ember.ArrayProxy.create({
-  content: []
+  content: [],
+
+  createModule: function(data) {
+    var module = YardViewer.Module.create(data);
+    this.pushObject(module);
+  }
 });
